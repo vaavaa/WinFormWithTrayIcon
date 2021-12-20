6425beff0175432894477cf6bbe591e31627137c
Andrey <vaa.andrey@gmail.com>
2021-12-20 20:05:35 +0600

    changes

--- WinFormWithTrayIcon/Form1.Designer.cs
+++ WinFormWithTrayIcon/Form1.Designer.cs
@@ -35,36 +35,50 @@
             this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.timeUpdater = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.time3 = new System.Windows.Forms.MaskedTextBox();
+            this.time2 = new System.Windows.Forms.MaskedTextBox();
+            this.time1 = new System.Windows.Forms.MaskedTextBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txtProgramTime = new System.Windows.Forms.TextBox();
+            this.txtGlobalTime = new System.Windows.Forms.TextBox();
+            this.txtLocalTime = new System.Windows.Forms.TextBox();
+            this.logUpdater = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.contextMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(93, 150);
+            this.btnOK.Location = new System.Drawing.Point(396, 41);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(138, 23);
+            this.btnOK.Size = new System.Drawing.Size(100, 23);
             this.btnOK.TabIndex = 0;
-            this.btnOK.Text = "&Close Form/App";
+            this.btnOK.Text = "Exit";
             this.btnOK.UseVisualStyleBackColor = true;
             this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
             // contextMenuStrip1
             // 
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.toolStripMenuItem1});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 32);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 10);
             this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip1_ItemClicked);
             // 
             // toolStripMenuItem1
             // 
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(57, 6);
             // 
             // notifyIcon1
             // 
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
-            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Icon = ((System.Drawing.Icon) (resources.GetObject("notifyIcon1.Icon")));
             this.notifyIcon1.Text = "WinFormWithTrayIcon";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
@@ -73,21 +87,156 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(396, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timeUpdater
+            // 
+            this.timeUpdater.Interval = 50;
+            this.timeUpdater.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.time3);
+            this.groupBox1.Controls.Add(this.time2);
+            this.groupBox1.Controls.Add(this.time1);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.txtProgramTime);
+            this.groupBox1.Controls.Add(this.txtGlobalTime);
+            this.groupBox1.Controls.Add(this.txtLocalTime);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(359, 97);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Time Flag";
+            // 
+            // time3
+            // 
+            this.time3.Location = new System.Drawing.Point(202, 67);
+            this.time3.Mask = "00:00:00:000";
+            this.time3.Name = "time3";
+            this.time3.Size = new System.Drawing.Size(141, 20);
+            this.time3.TabIndex = 11;
+            // 
+            // time2
+            // 
+            this.time2.Location = new System.Drawing.Point(202, 41);
+            this.time2.Mask = "00:00:00:000";
+            this.time2.Name = "time2";
+            this.time2.Size = new System.Drawing.Size(141, 20);
+            this.time2.TabIndex = 10;
+            // 
+            // time1
+            // 
+            this.time1.Location = new System.Drawing.Point(202, 16);
+            this.time1.Mask = "00:00:00:000";
+            this.time1.Name = "time1";
+            this.time1.Size = new System.Drawing.Size(141, 20);
+            this.time1.TabIndex = 9;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(16, 67);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(65, 17);
+            this.checkBox3.TabIndex = 8;
+            this.checkBox3.Text = "Program";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(16, 44);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(56, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Global";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(52, 17);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Local";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // txtProgramTime
+            // 
+            this.txtProgramTime.Location = new System.Drawing.Point(86, 67);
+            this.txtProgramTime.Name = "txtProgramTime";
+            this.txtProgramTime.ReadOnly = true;
+            this.txtProgramTime.Size = new System.Drawing.Size(110, 20);
+            this.txtProgramTime.TabIndex = 8;
+            // 
+            // txtGlobalTime
+            // 
+            this.txtGlobalTime.Location = new System.Drawing.Point(86, 41);
+            this.txtGlobalTime.Name = "txtGlobalTime";
+            this.txtGlobalTime.ReadOnly = true;
+            this.txtGlobalTime.Size = new System.Drawing.Size(110, 20);
+            this.txtGlobalTime.TabIndex = 7;
+            // 
+            // txtLocalTime
+            // 
+            this.txtLocalTime.Location = new System.Drawing.Point(86, 16);
+            this.txtLocalTime.Name = "txtLocalTime";
+            this.txtLocalTime.ReadOnly = true;
+            this.txtLocalTime.Size = new System.Drawing.Size(110, 20);
+            this.txtLocalTime.TabIndex = 6;
+            // 
+            // logUpdater
+            // 
+            this.logUpdater.Tick += new System.EventHandler(this.logUpdater_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 115);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(484, 367);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(324, 194);
+            this.ClientSize = new System.Drawing.Size(508, 494);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnOK);
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "SilkDragon";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-
         }
+
+        private System.Windows.Forms.Button button1;
 
         #endregion
 
@@ -96,6 +245,19 @@
         private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timeUpdater;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtProgramTime;
+        private System.Windows.Forms.TextBox txtGlobalTime;
+        private System.Windows.Forms.TextBox txtLocalTime;
+        private System.Windows.Forms.Timer logUpdater;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.MaskedTextBox time3;
+        private System.Windows.Forms.MaskedTextBox time2;
+        private System.Windows.Forms.MaskedTextBox time1;
     }
 }
 
--- WinFormWithTrayIcon/Form1.cs
+++ WinFormWithTrayIcon/Form1.cs
@@ -1,17 +1,33 @@
-﻿using System;
+﻿using FlaUI.Core.AutomationElements.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
+using FlaUI.Core.AutomationElements;
+using TextBox = FlaUI.Core.AutomationElements.TextBox;
+using GuerrillaNtp;
+using System.Net;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
 
 namespace WinFormWithTrayIcon
 {
     public partial class Form1 : Form
     {
-        bool Terminating = false;
+        private bool Terminating = false;
+        private bool GlobalTimeAsked = false;
+        private TimeSpan? offset = null;
+        private Logger _log = Global._log;
+        private List<Color> _logColors = Global._logColors;
+        private int RichTextLength = 0;
+        public Thread t;
+        private ExThreadButton exThreadButton;
+
 
         public Form1()
         {
@@ -24,6 +40,8 @@ namespace WinFormWithTrayIcon
             this.contextMenuStrip1.Items.Add("&Restore");
             this.contextMenuStrip1.Items.Add("-");
             this.contextMenuStrip1.Items.Add("E&xit");
+            timeUpdater.Start();
+            logUpdater.Start();
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -35,14 +53,18 @@ namespace WinFormWithTrayIcon
                 return;
             }
 
-            if (e.CloseReason == CloseReason.UserClosing && MessageBox.Show("Are you sure you want to close this form?", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.Cancel)
+            if (e.CloseReason == CloseReason.UserClosing && MessageBox.Show("Are you sure you want to close this form?",
+                    "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.Cancel)
             {
                 // only the user, selecting Cancel in a MessageBox, can do this.
                 e.Cancel = true;
             }
+
             if (!e.Cancel)
             {
                 // The application will shut down.
+                // Проверяем не запущена ли автоматизация, если запущена остнавливаем
+                if (Global.KeepCycleOn[0]) closeAutomatization();
 
                 // We cancel the shutdown, because the timer will do the shutdown when it fires.
                 // This will return to the app and allow the idle state to occur.
@@ -50,11 +72,11 @@ namespace WinFormWithTrayIcon
 
                 // Dispose of the tray icon this way.
                 this.notifyIcon1.Dispose();
-                
+
                 // Set the termination flag so that the next entry into this event will
                 // not be cancelled.
                 Terminating = true;
-                
+
                 // Activate the timer to fire
                 this.timer1.Interval = 100;
                 this.timer1.Enabled = true;
@@ -105,6 +127,261 @@ namespace WinFormWithTrayIcon
         {
             Show();
             WindowState = FormWindowState.Normal;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!Global.KeepCycleOn[0])
+            {
+                Global.KeepCycleOn[0] = true;
+                exThreadButton = new ExThreadButton("ThreadButton ");
+                this.button1.Text = "Stop!";
+            }
+            else
+            {
+                closeAutomatization();
+            }
+        }
+
+        private void closeAutomatization()
+        {
+            Global.KeepCycleOn[0] = false;
+            _log.AddToLog("Stopping automatization process...", _logColors[0]);
+            exThreadButton.thr.Abort(100);
+            exThreadButton.thr.Join();
+            _log.AddToLog("Automatization process just stopped", _logColors[0]);
+            this.button1.Text = "Start";
+        }
+
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (!GlobalTimeAsked)
+            {
+                try
+                {
+                    using (var ntp = new NtpClient(Dns.GetHostAddresses("pool.ntp.org")[0]))
+                        offset = ntp.GetCorrectionOffset();
+                    GlobalTimeAsked = true;
+                }
+                catch (System.Net.Sockets.SocketException)
+                {
+                }
+            }
+
+            if (offset == null)
+            {
+                offset = TimeSpan.FromSeconds(-2);
+            }
+
+            // use the offset throughout your app
+            var accurateTime = DateTime.UtcNow + offset;
+            var localtime = DateTime.Now;
+            this.txtLocalTime.Text = localtime.ToString();
+            this.txtGlobalTime.Text = accurateTime.ToString();
+            Global.timesSystem[0] = localtime;
+            Global.timesSystem[1] = accurateTime.Value;
+            //Global.times[2] = 
+        }
+
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void logUpdater_Tick(object sender, EventArgs e)
+        {
+            String LogValue = _log.GetLogAsRichText(false);
+            if (LogValue != null && LogValue.Length != RichTextLength)
+            {
+                richTextBox1.Rtf = LogValue;
+                richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                // scroll it automatically
+                richTextBox1.ScrollToCaret();
+                RichTextLength = LogValue.Length;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var now = CSharpScript
+                .EvaluateAsync<bool>("WinFormWithTrayIcon.Global.times[0] < new System.DateTime(2021,12,20,17,15,0,0)")
+                .Result;
+            MessageBox.Show(now.ToString());
+        }
+
+        private Environment GetEnvironment()
+        {
+            Action[] actions = new Action[2];
+
+            actions[0] = new Action
+            {
+                ActionCondition = "None",
+                ActionType = "LaunchApp",
+                ActionValue = "C:\\Program Files (x86)\\ETCPlaza\\launcher.exe"
+            };
+
+            actions[1] = new Action
+            {
+                ActionCondition = "None",
+                ActionType = "wait",
+                ActionValue = "5000"
+            };
+
+            SequenceBody[] sequences = new SequenceBody[1];
+            sequences[0] = new SequenceBody
+            {
+                Number = 0,
+                PointType = "None",
+                PointName = "",
+                PointValue = "C:\\Program Files (x86)\\ETCPlaza",
+                PointActions = actions
+            };
+
+            AutoWindow[] windows = new AutoWindow[1];
+            windows[0] = new AutoWindow
+            {
+                Order = 100,
+                Name = "NoneWindow",
+                Sequence = sequences
+            };
+
+            var obj = new Environment
+            {
+                Variables = "me=1900;tpl=8090;fpl='dddddd';noway='80'",
+                Windows = windows
+            };
+
+            return obj;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!this.time3.ReadOnly)
+            {
+                if (this.time3.Text.Length != 12)
+                {
+                    checkBox3.Checked = false;
+                    MessageBox.Show("Необходимо ввести полное время запуска.", "Внимание");
+                    return;
+                }
+                else
+                {
+                    string[] exactTime = this.time3.Text.Split(':');
+                    int HH = int.Parse(exactTime[0]);
+                    int mm = int.Parse(exactTime[1]);
+                    int ss = int.Parse(exactTime[2]);
+                    int msec = int.Parse(exactTime[3]);
+                    
+                    //Рубиконом является 11:00
+                    TimeSpan start = new TimeSpan(11, 0, 0); //11 o'clock
+                    TimeSpan now = DateTime.Now.TimeOfDay;
+                    int plus = 0;
+                    if (now > start) plus = 0;
+                    try
+                    {
+                        DateTime runTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, HH, mm, ss, msec);
+                        runTime = runTime.AddDays(plus);
+                        Global.timesSet[2] = runTime;
+                        _log.AddToLog("Program running time set to " + runTime.ToString("dd.MM.yyyy:HH.mm.ss.fff"),
+                            _logColors[1]);
+                        this.time3.ReadOnly = true;
+                    }
+                    catch (Exception)
+                    {
+                        checkBox3.Checked = false;
+                        MessageBox.Show("Необходимо ввести корректное время запуска.", "Внимание");
+                    }
+
+                }
+            }
+            else this.time3.ReadOnly = false;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!this.time1.ReadOnly)
+            {
+                if (this.time1.Text.Length != 12)
+                {
+                    checkBox1.Checked = false;
+                    MessageBox.Show("Необходимо ввести полное время запуска.", "Внимание");
+                    return;
+                }
+                else
+                {
+                    string[] exactTime = this.time1.Text.Split(':');
+                    int HH = int.Parse(exactTime[0]);
+                    int mm = int.Parse(exactTime[1]);
+                    int ss = int.Parse(exactTime[2]);
+                    int msec = int.Parse(exactTime[3]);
+                    
+                    //Рубиконом является 11:00
+                    TimeSpan start = new TimeSpan(11, 0, 0); //11 o'clock
+                    TimeSpan now = DateTime.Now.TimeOfDay;
+                    int plus = 0;
+                    if (now > start) plus = 0;
+                    try
+                    {
+                        DateTime runTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, HH, mm, ss, msec);
+                        runTime = runTime.AddDays(plus);
+                        Global.timesSet[0] = runTime;
+                        _log.AddToLog("Local running time set to " + runTime.ToString("dd.MM.yyyy:HH.mm.ss.fff"),
+                            _logColors[1]);
+                        this.time1.ReadOnly = true;
+                    }
+                    catch (Exception)
+                    {
+                        checkBox1.Checked = false;
+                        MessageBox.Show("Необходимо ввести корректное время запуска.", "Внимание");
+                    }
+
+                }
+            }
+            else this.time1.ReadOnly = false;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!this.time2.ReadOnly)
+            {
+                if (this.time2.Text.Length != 12)
+                {
+                    checkBox2.Checked = false;
+                    MessageBox.Show("Необходимо ввести полное время запуска.", "Внимание");
+                    return;
+                }
+                else
+                {
+                    string[] exactTime = this.time2.Text.Split(':');
+                    int HH = int.Parse(exactTime[0]);
+                    int mm = int.Parse(exactTime[1]);
+                    int ss = int.Parse(exactTime[2]);
+                    int msec = int.Parse(exactTime[3]);
+                    
+                    //Рубиконом является 11:00
+                    TimeSpan start = new TimeSpan(11, 0, 0); //11 o'clock
+                    TimeSpan now = DateTime.Now.TimeOfDay;
+                    int plus = 0;
+                    if (now > start) plus = 0;
+                    try
+                    {
+                        DateTime runTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, HH, mm, ss, msec);
+                        runTime = runTime.AddDays(plus);
+                        Global.timesSet[1] = runTime;
+                        _log.AddToLog("Global running time set to " + runTime.ToString("dd.MM.yyyy:HH.mm.ss.fff"),
+                            _logColors[1]);
+                        this.time2.ReadOnly = true;
+                    }
+                    catch (Exception)
+                    {
+                        checkBox2.Checked = false;
+                        MessageBox.Show("Необходимо ввести корректное время запуска.", "Внимание");
+                    }
+
+                }
+            }
+            else this.time2.ReadOnly = false;
         }
     }
 }
